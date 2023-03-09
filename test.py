83348578b2a2bab31c8b12c636b52c7aa80dcc8a
Louise Doran <louise@louisedoran.net>
2023-03-09 11:15:03 +0000

    File to test pylint with

---
+++ test.py
@@ -0,0 +1,16 @@
+import urllib
+from itertools import chain, combinations, cycle, groupby, ifilter, ifilterfalse, imap, izip, tee, takewhile, starmap
+
+
+def first_function(a, b, c, d=None):
+    return (a + b + d)
+
+def second_function():
+    return "testing" # a test
+
+
+
+def third_function(first_arg, second_arg, third_arg, fourth_arg, fifth_arg, sixth_arg, seventh_arg, eighth_arg):
+    return 5
+
+
